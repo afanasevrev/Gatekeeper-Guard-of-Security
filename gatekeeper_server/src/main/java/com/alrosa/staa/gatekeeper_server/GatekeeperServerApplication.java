6e5629dfb1814795193484df8fcb937b43a22ac7
revoly afanasev <rewoly@mail.ru>
2024-01-21 23:29:41 +0900

    first commit

---
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/GatekeeperServerApplication.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperServerApplication.class, args);
+	}
+
+}
