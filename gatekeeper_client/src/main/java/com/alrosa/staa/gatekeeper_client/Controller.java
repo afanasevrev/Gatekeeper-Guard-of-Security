c58c26cdadccda51e6d6c9ac0f87712be4a48803
revoly afanasev <rewoly@mail.ru>
2024-01-22 21:42:58 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/Controller.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/Controller.java
@@ -3,7 +3,7 @@ package com.alrosa.staa.gatekeeper_client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
