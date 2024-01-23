cce5761abbd25f6d7e1729120f0cd4f457dbeaad
unknown <rewoly@mail.ru>
2024-01-23 17:23:19 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/GateKeeperClient.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/GateKeeperClient.java
@@ -12,7 +12,7 @@ public class GateKeeperClient extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GateKeeperClient.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("GateKeeper - Guard of Security");
         stage.setScene(scene);
         stage.show();
     }
