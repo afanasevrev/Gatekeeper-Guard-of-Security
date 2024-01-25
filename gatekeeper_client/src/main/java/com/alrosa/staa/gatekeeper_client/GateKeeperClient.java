c5ff37e9bc5b5a8a152af66a0c79ab724bb5f96c
revoly afanasev <rewoly@mail.ru>
2024-01-25 21:29:52 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/GateKeeperClient.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/GateKeeperClient.java
@@ -5,16 +5,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+/**
+ * Точка входа в приложение
+ */
 public class GateKeeperClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GateKeeperClient.class.getResource("sign_in.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 350);
         stage.setTitle("GateKeeper - Guard of Security");
-        //scene.getStylesheets().add(GateKeeperClient.class.getResource("css/styles.css").toExternalForm());
         stage.getIcons().add(Variables.shieldImage);
         stage.setResizable(false);
         stage.setScene(scene);
