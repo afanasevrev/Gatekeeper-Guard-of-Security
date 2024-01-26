0ef12c1f2286d3fcd066514af047b18f65776e7c
unknown <rewoly@mail.ru>
2024-01-26 11:36:40 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.alrosa.staa.gatekeeper_client.controller;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class Controller {
-
-}
---
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/SignInController.java
@@ -0,0 +1,23 @@
+package com.alrosa.staa.gatekeeper_client.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Контроллер предназначен для работы с файлом sign_in.fxml
+ */
+public class SignInController {
+    @FXML
+    private TextField loginTextField = new TextField();
+    @FXML
+    private PasswordField passwordField = new PasswordField();
+    @FXML
+    private Button signInButton = new Button();
+    @FXML
+    private TextArea logsTextArea = new TextArea();
+    
+
+}
