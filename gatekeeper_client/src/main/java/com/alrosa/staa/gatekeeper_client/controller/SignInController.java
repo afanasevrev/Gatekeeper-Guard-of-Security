5d015e8752d9a1e7224edf99642fa0606d2a9ce2
unknown <rewoly@mail.ru>
2024-01-29 17:20:58 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/SignInController.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/SignInController.java
@@ -10,11 +10,11 @@ import javafx.scene.control.TextField;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Base64;
 import java.util.ResourceBundle;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Контроллер предназначен для работы с файлом sign_in.fxml
@@ -32,20 +32,58 @@ public class SignInController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         try {
-            url = new URL("http://localhost:8080/");
-            URLConnection connection = url.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String text = in.readLine();
-            logsTextArea.setText(text);
+            authentication();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
-    public boolean authentication() throws IOException, InterruptedException {
+    public void authentication() throws IOException, InterruptedException {
+        try {
+            // URL, на который отправляем запрос
+            String url = "http://localhost:8080/";
 
-        return false;
+            // Создаем объект URL
+            URL obj = new URL(url);
+
+            // Открываем соединение
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // Устанавливаем метод запроса
+            con.setRequestMethod("GET");
+
+            // Устанавливаем заголовок для basic авторизации
+            String username = "root";
+            String password = "gatekeeper";
+            String credentials = username + ":" + password;
+            byte[] authEncBytes = Base64.getEncoder().encode(credentials.getBytes());
+            String authStringEnc = new String(authEncBytes);
+            con.setRequestProperty("Authorization", "Basic " + authStringEnc);
+
+            // Получаем ответ
+            int responseCode = con.getResponseCode();
+            System.out.println("Отправляем GET запрос на " + url);
+            System.out.println("Ответ: " + responseCode);
+
+            // Считываем ответ
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Выводим ответ
+            System.out.println(response.toString());
+            logsTextArea.setText(response.toString());
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        }
     }
 }
--- gatekeeper_client/src/main/java/module-info.java
+++ gatekeeper_client/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.alrosa.staa.gatekeeper_client {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
-    requires spring.beans;
-
 
     opens com.alrosa.staa.gatekeeper_client to javafx.fxml;
     exports com.alrosa.staa.gatekeeper_client;
