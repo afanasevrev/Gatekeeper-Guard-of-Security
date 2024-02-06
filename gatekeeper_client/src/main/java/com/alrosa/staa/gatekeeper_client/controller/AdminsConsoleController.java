41dd9eff1c1cd861fffd003202fc84ad3cf8d897
unknown <rewoly@mail.ru>
2024-02-06 16:31:29 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/AdminsConsoleController.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/AdminsConsoleController.java
@@ -1,6 +1,11 @@
 package com.alrosa.staa.gatekeeper_client.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,8 +14,45 @@ import java.util.ResourceBundle;
  * Контроллер предназначен для работы с файлом admins_console.fxml
  */
 public class AdminsConsoleController implements Initializable {
+    //Добавим контекстное меню
+    private ContextMenu contextMenu = new ContextMenu();
+    //Создание кнопки "Добавить"
+    private MenuItem menuAdd = new MenuItem("Добавить");
+    //Создание кнопки "Удалить"
+    private MenuItem menuDelete = new MenuItem("Удалить");
+    //Окно для отображения дерева
+    @FXML
+    private AnchorPane windowTree = new AnchorPane();
+    //Горизонтальный сплиттер
+    @FXML
+    private SplitPane horizontal = new SplitPane();
+    //Вертикальный сплиттер
+    @FXML
+    private SplitPane vertical = new SplitPane();
+    //Вертикальный бокс
+    @FXML
+    private VBox vBox = new VBox();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //В контекстное меню добавляем кнопки
+        contextMenu.getItems().addAll(menuAdd, menuDelete);
+        //В наше дерево добавляем контекстное меню
+        //treeView.setContextMenu(contextMenu);
+
+        AnchorPane.setLeftAnchor(vBox, 0.0);
+        AnchorPane.setBottomAnchor(vBox, 0.0);
+        AnchorPane.setTopAnchor(vBox, 0.0);
+        AnchorPane.setRightAnchor(vBox, 0.0);
+
+        AnchorPane.setLeftAnchor(horizontal, 0.0);
+        AnchorPane.setBottomAnchor(horizontal, 0.0);
+        AnchorPane.setTopAnchor(horizontal, 0.0);
+        AnchorPane.setRightAnchor(horizontal, 0.0);
+
+        AnchorPane.setLeftAnchor(vertical, 0.0);
+        AnchorPane.setBottomAnchor(vertical, 0.0);
+        AnchorPane.setTopAnchor(vertical, 0.0);
+        AnchorPane.setRightAnchor(vertical, 0.0);
 
     }
 }
