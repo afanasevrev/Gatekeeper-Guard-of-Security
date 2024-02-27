98cc60f93f3c251770010c8fd1a061f051b93f38
unknown <rewoly@mail.ru>
2024-02-27 17:24:39 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/AdminsConsoleController.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/AdminsConsoleController.java
@@ -65,9 +65,9 @@ public class AdminsConsoleController implements Initializable {
         // Создаем HttpHeaders и добавляем куки
         HttpHeaders headers = new HttpHeaders();
         headers.set("Cookie", Variables.jSessionId);
-        
+        headers.setBasicAuth("root", "gatekeeper");
         // Отправляем запрос с заданными куками
-        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/authenticate", HttpMethod.GET, new HttpEntity<>(headers), String.class);
 
         // Обработка ответа на запрос
         System.out.println("Response: " + response.getBody());
