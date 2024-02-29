0e0aebb825d2e6175ab1bf885bef624eed16f9b1
unknown <rewoly@mail.ru>
2024-02-29 12:21:11 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/security/SecurityConfig.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.alrosa.staa.gatekeeper_server.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,8 +11,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     private final PasswordEncoder pwEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
