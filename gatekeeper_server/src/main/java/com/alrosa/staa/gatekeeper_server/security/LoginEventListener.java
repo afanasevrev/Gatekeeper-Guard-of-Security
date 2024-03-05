1f61254d84f88596720e474caab6eccc4c37c27e
unknown <rewoly@mail.ru>
2024-03-05 09:34:12 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/security/LoginEventListener.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/security/LoginEventListener.java
@@ -7,24 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Класс нужен для фиксации логинов авторизованных клиентов
+ * Класс нужен для фиксации авторизованных клиентов
  */
 @Component
 public class LoginEventListener {
+    //Массив для хранения логинов
     public static List<String> successfulLogins = new ArrayList<>();
-
+    //Записываем роль подключенного клиента
     public static String getAuthorities;
 
     @EventListener
     public void handleSuccessfulLogin(AuthenticationSuccessEvent event) {
         String username = event.getAuthentication().getName();
         successfulLogins.add(username);
-        System.out.println("Successful login for user: " + username);
         System.out.println(event.getAuthentication().getDetails());
         getAuthorities = String.valueOf(event.getAuthentication().getAuthorities());
-    }
-
-    public List<String> getSuccessfulLogins() {
-        return successfulLogins;
     }
 }
