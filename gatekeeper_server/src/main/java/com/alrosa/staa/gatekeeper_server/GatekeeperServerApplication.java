e711058d3ac121462a830adc19ae2b632ba11eb1
unknown <rewoly@mail.ru>
2024-03-05 16:20:10 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/GatekeeperServerApplication.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/GatekeeperServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatekeeperServerApplication {
 	public static void main(String[] args) throws Exception {
+		//Запускаем сервер Spring
 		SpringApplication.run(GatekeeperServerApplication.class, args);
 		//Создаем экземпляр ресивера
 		Receiver receiver = new Receiver();
