41d041aba812708a5f699b1e09eebc5bb3642dc1
unknown <rewoly@mail.ru>
2024-03-06 17:17:35 +0900

    first commit

---
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/rabbitmq/Receiver.java
@@ -0,0 +1,29 @@
+package com.alrosa.staa.gatekeeper_client.controller.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+public class Receiver {
+    private static final String QUEUE_NAME = "Gatekeeper";
+
+    public void start() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Server RabbitMQ is started");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
