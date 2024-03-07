5fdf05bae96dbdf3955b28f59f730d57d0a34b2c
unknown <rewoly@mail.ru>
2024-03-07 12:20:17 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/session/Receiver.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/session/Receiver.java
@@ -7,6 +7,9 @@ import com.rabbitmq.client.DeliverCallback;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Класс приёмник сообщений от сервера
+ */
 public class Receiver {
     private static final String QUEUE_NAME = "Gatekeeper";
 
