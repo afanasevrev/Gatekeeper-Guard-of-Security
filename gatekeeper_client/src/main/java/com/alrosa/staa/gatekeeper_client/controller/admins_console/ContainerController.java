da1a6ab84bf3eea2d6af59e62b67495a94f3bc8c
unknown <rewoly@mail.ru>
2024-03-11 17:20:50 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/admins_console/ContainerController.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/admins_console/ContainerController.java
@@ -1,4 +1,19 @@
 package com.alrosa.staa.gatekeeper_client.controller.admins_console;
 
-public class ContainerController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Класс - контроллер для контейнера
+ * В контейнере будем выбирать объекты
+ * и добавлять их в дерево системы
+ */
+public class ContainerController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
