3b82d177bb3883c28e4a5283808df55b5ed3bd17
unknown <rewoly@mail.ru>
2024-03-12 16:16:28 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/admins_console/ContainerController.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/admins_console/ContainerController.java
@@ -1,6 +1,10 @@
 package com.alrosa.staa.gatekeeper_client.controller.admins_console;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,9 +15,25 @@ import java.util.ResourceBundle;
  * и добавлять их в дерево системы
  */
 public class ContainerController implements Initializable {
-    
+    @FXML
+    private AnchorPane anchorPane = new AnchorPane();
+    @FXML
+    private Button buttonCreate = new Button();
+    @FXML
+    private Button buttonCancel = new Button();
+    //Инициализируем дерево в контейнере
+    private TreeView container = new TreeView();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private void isPressedButtonCreate() {}
+
+    @FXML
+    private void isPressedButtonCancel() {
 
     }
 }
