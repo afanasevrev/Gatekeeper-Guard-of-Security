0a25b47c10e68d7dd7a2b86b83c3499c4bbdd34b
unknown <rewoly@mail.ru>
2024-03-13 10:25:12 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/Variables.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/Variables.java
@@ -1,6 +1,8 @@
 package com.alrosa.staa.gatekeeper_client.model;
 
 import com.alrosa.staa.gatekeeper_client.GateKeeperClient;
+import com.alrosa.staa.gatekeeper_client.model.tree_objects.Global;
+import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 
 import java.io.FileInputStream;
@@ -17,6 +19,14 @@ public class Variables {
     public static String jSessionId;
     //Указываем путь к настройкам
     public static final Properties properties = new Properties();
+    //Добавляем вершину дерева для контейнера
+    public static TreeItem<Global> containerConsoleItem;
+    //Добавляем вершину дерева для админского консоля
+    public static TreeItem<Global> adminsConsoleItem;
+    //Добавляем статический enum для контейнера
+    public static Direction containerConsoleDirection = Direction.MAIN;
+    //Добавляем статический enum для админского консоля
+    public static Direction adminsConsoleDirection = Direction.MAIN;
     //Указываем путь к рисунку shield. Значок.
     public static final Image shieldImage = new Image(GateKeeperClient.class.getResource("favicon/shield.png").toString());
     //Указываем путь к рисунку main.
