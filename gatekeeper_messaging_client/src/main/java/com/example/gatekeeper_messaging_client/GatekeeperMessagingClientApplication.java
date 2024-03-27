22f2b2142ff8d5e2f518737592fbbbafbed4a845
unknown <rewoly@mail.ru>
2024-03-27 17:06:52 +0900

    first commit

---
+++ gatekeeper_messaging_client/src/main/java/com/example/gatekeeper_messaging_client/GatekeeperMessagingClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatekeeper_messaging_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperMessagingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperMessagingClientApplication.class, args);
+	}
+
+}
---
+++ gatekeeper_messaging_client/src/test/java/com/example/gatekeeper_messaging_client/GatekeeperMessagingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatekeeper_messaging_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperMessagingClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
