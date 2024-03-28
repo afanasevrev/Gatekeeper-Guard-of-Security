bb5f37e4c47c664a275b7ba44b11b03295881fa0
unknown <rewoly@mail.ru>
2024-03-28 17:23:16 +0900

    first commit

--- gatekeeper_messaging_client/src/main/java/com/example/gatekeeper_messaging_client/GatekeeperMessagingClientApplication.java
+++ gatekeeper_messaging_client/src/main/java/com/example/gatekeeper_messaging_client/GatekeeperMessagingClientApplication.java
@@ -1,13 +1,6 @@
 package com.example.gatekeeper_messaging_client;
 
 import org.apache.log4j.BasicConfigurator;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 public class GatekeeperMessagingClientApplication {
 
