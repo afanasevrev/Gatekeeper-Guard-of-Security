4afdd6e97e2efa29d04281cd6453626fa2b075fe
unknown <rewoly@mail.ru>
2024-03-28 17:12:28 +0900

    first commit

--- gatekeeper_messaging_client/src/main/java/com/example/gatekeeper_messaging_client/SendMessage.java
+++ gatekeeper_messaging_client/src/main/java/com/example/gatekeeper_messaging_client/SendMessage.java
@@ -1,19 +1,15 @@
 package com.example.gatekeeper_messaging_client;
 
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-@AutoConfiguration
-@ComponentScan
-@Import(RabbitConfiguration.class)
+import org.springframework.stereotype.Component;
+
+@Component
 public class SendMessage {
+
     @Autowired
-    AmqpTemplate template;
+    RabbitTemplate template;
 
     public String send(String text) {
         String response = (String) template.convertSendAndReceive(text);
