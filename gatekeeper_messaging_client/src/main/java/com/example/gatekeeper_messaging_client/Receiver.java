2ac8af243685cc4f1ece230e6d8d204ab57e6085
revoly afanasev <rewoly@mail.ru>
2024-03-31 22:47:21 +0900

    first commit

---
+++ gatekeeper_messaging_client/src/main/java/com/example/gatekeeper_messaging_client/Receiver.java
@@ -0,0 +1,12 @@
+package com.example.gatekeeper_messaging_client;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Класс для приёма сообщений от сервера
+ */
+public class Receiver {
+    ConnectionFactory connectionFactory = new ConnectionFactory();
+
+
+}
