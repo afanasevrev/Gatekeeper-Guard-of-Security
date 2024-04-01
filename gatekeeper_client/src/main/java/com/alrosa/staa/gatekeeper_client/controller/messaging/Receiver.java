96c6cb119db69e89488189e013743f094d9c3190
unknown <rewoly@mail.ru>
2024-04-01 12:22:06 +0900

    first commit

---
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/messaging/Receiver.java
@@ -0,0 +1,7 @@
+package com.alrosa.staa.gatekeeper_client.controller.messaging;
+/**
+ * Класс Приёмник сообщений от сервера
+ */
+public class Receiver {
+    
+}
---
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/controller/messaging/Transmitter.java
@@ -0,0 +1,8 @@
+package com.alrosa.staa.gatekeeper_client.controller.messaging;
+
+/**
+ * Класс отвечает за отправку сообщений на сервер
+ */
+public class Transmitter {
+
+}
