f18a8009e64b75db8ad86b27da6e8bb66e47b3ed
unknown <rewoly@mail.ru>
2024-04-01 11:53:02 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/GatekeeperServerApplication.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/GatekeeperServerApplication.java
@@ -1,14 +1,23 @@
 package com.alrosa.staa.gatekeeper_server;
 
+import com.alrosa.staa.gatekeeper_server.sessions.RabbitConfiguration;
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * Точка запуска сервера
  */
 @SpringBootApplication
+@Import(RabbitConfiguration.class)
+@EnableAutoConfiguration
+@ComponentScan
 public class GatekeeperServerApplication {
 	public static void main(String[] args) throws Exception {
+		BasicConfigurator.configure();
 		//Запускаем сервер Spring
 		SpringApplication.run(GatekeeperServerApplication.class, args);
 	}
