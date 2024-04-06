976523d77c82b614625a3caf8011134c4e53cc1d
revoly afanasev <rewoly@mail.ru>
2024-04-07 01:00:54 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/Direction.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/Direction.java
@@ -1,8 +1,32 @@
 package com.alrosa.staa.gatekeeper_server.db;
 
 public enum Direction {
+    //Главный объект системы
     MAIN,
-    SERVER,
-    COMPUTER,
-    BUREAU
+    //Объекты MAIN
+    BUREAU, COMPUTER, SERVER,
+    //Объекты BUREAU
+    USERS, ADMINS, OPERATORS, GLOBAL_ACCESS_LEVELS, CARDS, CARD_LAYOUTS, POSITIONS, ORGANIZATIONS,
+    //Объекты SERVER
+    PERCO,
+    //Объекты PERCO
+    PERCOC01,
+    //Объекты PERCOC01
+    CARDREADER,
+    //Объекты COMPUTER
+    CONSOLE,
+    //Объекты USERS, ADMINS, OPERATORS
+    MAN, WOMAN,
+    //Объекты GLOBAL_ACCESS_LEVELS
+    GLOBAL_ACCESS_LEVEL,
+    //Объекты CARDS
+    CARD,
+    //Объекты CARD_LAYOUTS
+    CARD_LAYOUT,
+    //Объекты POSITIONS
+    POSITION,
+    //Объекты ORGANIZATIONS
+    ORGANIZATION,
+    //Объекты ORGANIZATION
+    OFFICE;
 }
