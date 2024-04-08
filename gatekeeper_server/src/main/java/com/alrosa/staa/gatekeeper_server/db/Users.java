aeca29746f05cde26709448e9937e2d90152c520
unknown <rewoly@mail.ru>
2024-04-08 10:12:24 +0900

    first commit

---
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/Users.java
@@ -0,0 +1,25 @@
+package com.alrosa.staa.gatekeeper_server.db;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "gk_users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "users_name")
+    private String users_name;
+    @Column(name = "parent_id")
+    private int parent_id;
+    public Users(){}
+    public Users(String users_name, int parent_id){
+        this.users_name = users_name;
+        this.parent_id = parent_id;
+    }
+}
