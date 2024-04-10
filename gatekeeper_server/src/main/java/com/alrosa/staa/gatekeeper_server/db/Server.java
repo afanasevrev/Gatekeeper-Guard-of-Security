f3b9d77608ef63f69eddb2f4efc3c1d88be5c816
unknown <rewoly@mail.ru>
2024-04-10 09:28:02 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/Server.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/Server.java
@@ -1,7 +1,11 @@
 package com.alrosa.staa.gatekeeper_server.db;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "gk_servers")
 public class Server {
@@ -9,53 +13,16 @@ public class Server {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-
     @Column(name = "server_name")
     private String server_name;
-
     @Column(name = "server_ip")
     private String server_ip;
-
     @Column(name = "parent_id")
     private int parent_id;
-
     public Server(){}
-
     public Server(String server_name, String server_ip, int parent_id) {
         this.server_name = server_name;
         this.server_ip = server_ip;
-        this.parent_id = parent_id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getServer_name() {
-        return server_name;
-    }
-
-    public void setServer_name(String server_name) {
-        this.server_name = server_name;
-    }
-
-    public String getServer_ip() {
-        return server_ip;
-    }
-
-    public void setServer_ip(String server_ip) {
-        this.server_ip = server_ip;
-    }
-
-    public int getParent_id() {
-        return parent_id;
-    }
-
-    public void setParent_id(int parent_id) {
         this.parent_id = parent_id;
     }
 }
