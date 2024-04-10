1c3d7b4e02390470c540ffdc802c3b0f1e2d26a6
unknown <rewoly@mail.ru>
2024-04-10 17:02:51 +0900

    first commit

---
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/Organizations.java
@@ -0,0 +1,27 @@
+package com.alrosa.staa.gatekeeper_server.db;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "gk_organizations")
+public class Organizations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "organizations_name")
+    private String organizations_name;
+    @Column(name = "parent_id")
+    private int parent_id;
+    public Organizations(){}
+    public Organizations(String organizations_name, int parent_id) {
+        this.organizations_name = organizations_name;
+        this.parent_id = parent_id;
+    }
+}
