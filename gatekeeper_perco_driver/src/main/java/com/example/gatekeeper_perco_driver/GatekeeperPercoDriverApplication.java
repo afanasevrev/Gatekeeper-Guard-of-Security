c109c8672e430f15f04343afd7c2f811161b3cf3
revoly afanasev <rewoly@mail.ru>
2024-04-11 00:11:01 +0900

    first commit

---
+++ gatekeeper_perco_driver/src/main/java/com/example/gatekeeper_perco_driver/GatekeeperPercoDriverApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatekeeper_perco_driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperPercoDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperPercoDriverApplication.class, args);
+	}
+
+}
---
+++ gatekeeper_perco_driver/src/test/java/com/example/gatekeeper_perco_driver/GatekeeperPercoDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatekeeper_perco_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperPercoDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
