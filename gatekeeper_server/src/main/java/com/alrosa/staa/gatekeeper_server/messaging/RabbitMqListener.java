42761689d0c0bf3f61167000067fdab422c97b17
unknown <rewoly@mail.ru>
2024-04-11 17:20:28 +0900

    first commit

--- gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/messaging/RabbitMqListener.java
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/messaging/RabbitMqListener.java
@@ -236,6 +236,7 @@ public class RabbitMqListener {
                 general.setDirection(Direction.CARDREADER);
                 text = gson.toJson(general);
                 template.convertAndSend(Variables.QUEUE_NAME_1, text);
+                break;
             default:
                 template.convertAndSend(Variables.QUEUE_NAME_1, "Этот вопрос ещё не проработан");
                 break;
