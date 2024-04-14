df1a77c75f7b93af6f926878ea3704bdae2ad36f
revoly afanasev <rewoly@mail.ru>
2024-04-15 00:02:43 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/GateKeeperClient.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/GateKeeperClient.java
@@ -23,7 +23,6 @@ public class GateKeeperClient extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         BasicConfigurator.configure();
         launch();
