4f09533785e5bf6aede93e24b4ace56aa3b68144
revoly afanasev <rewoly@mail.ru>
2024-04-14 23:33:49 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/Roles.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/Roles.java
@@ -1,18 +1,16 @@
 package com.alrosa.staa.gatekeeper_client.model;
 
+import lombok.Setter;
+import lombok.Getter;
 /**
  * Класс для приёма роли от сервера
  */
+@Setter
+@Getter
 public class Roles {
     private String role;
     public Roles(){}
     public Roles(String role) {
-        this.role = role;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
         this.role = role;
     }
 }
--- gatekeeper_client/src/main/java/module-info.java
+++ gatekeeper_client/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.alrosa.staa.gatekeeper_client {
     requires com.rabbitmq.client;
     requires com.google.gson;
     requires log4j;
+    requires lombok;
 
     opens com.alrosa.staa.gatekeeper_client to javafx.fxml;
     exports com.alrosa.staa.gatekeeper_client;
