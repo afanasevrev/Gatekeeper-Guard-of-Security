dad95d7ad74f29cd31bdae2deffd304988b3df9b
unknown <rewoly@mail.ru>
2024-04-16 02:34:51 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/CommandList.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/CommandList.java
@@ -4,6 +4,5 @@ package com.alrosa.staa.gatekeeper_client.model;
  * Здесь будет список команд для общения с сервером
  */
 public enum CommandList {
-    //Обновить
-    UPDATE
+    UPDATE, CREATE, DELETE
 }
