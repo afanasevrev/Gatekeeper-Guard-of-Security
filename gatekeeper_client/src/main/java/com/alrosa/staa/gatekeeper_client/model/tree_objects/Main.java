cd7217626a7b46d6ce5bb79cfc8435706c140c0a
unknown <rewoly@mail.ru>
2024-04-17 03:27:56 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/Main.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/Main.java
@@ -8,61 +8,47 @@ import com.alrosa.staa.gatekeeper_client.model.Direction;
 public class Main implements Global {
     //ID объекта в БД
     private int id = 1;
-
     //Родительский ID объекта в БД
     private int parentId = 0;
-
     //Имя объекта
     private String complete_name = "Главный";
-
     private Direction direction = Direction.MAIN;
-
     public Main(){}
-
     public Main(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setId(int id) {
         this.id = id;
     }
-
     @Override
     public void setParentId(int parentId) {
         this.parentId = parentId;
     }
-
     @Override
     public int getId() {
         return this.id;
     }
-
     @Override
     public int getParentId() {
         return this.parentId;
     }
-
     @Override
     public String getComplete_name() {
         return complete_name;
     }
-
     @Override
     public void setComplete_name(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
-
     @Override
     public Direction getDirection() {
         return this.direction;
     }
-
     @Override
     public String toString() {
         return getComplete_name();
