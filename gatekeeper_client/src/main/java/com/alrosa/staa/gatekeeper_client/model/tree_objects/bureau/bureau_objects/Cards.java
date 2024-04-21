e41901e827987a7a508a9eb86db04f0336c32d50
revoly afanasev <rewoly@mail.ru>
2024-04-22 00:19:22 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/bureau/bureau_objects/Cards.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/bureau/bureau_objects/Cards.java
@@ -2,53 +2,41 @@ package com.alrosa.staa.gatekeeper_client.model.tree_objects.bureau.bureau_objec
 
 import com.alrosa.staa.gatekeeper_client.model.Direction;
 import com.alrosa.staa.gatekeeper_client.model.tree_objects.Global;
-
 /**
  * Класс для карт доступа
  */
 public class Cards implements Global {
     //ID объекта в БД
     private int id;
-
     //Родительский ID объекта в БД
     private int parentId;
-
     //Имя объекта
     private String complete_name = "Карты доступа";
-
     private Direction direction = Direction.CARDS;
-
     public Cards() {}
-
     public Cards(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setId(int id) {
         this.id = id;
     }
-
     @Override
     public void setParentId(int parentId) {
         this.parentId = parentId;
     }
-
     @Override
     public int getId() {
         return this.id;
     }
-
     @Override
     public int getParentId() {
         return this.parentId;
     }
-
     @Override
     public String getComplete_name() {
         return complete_name;
     }
-
     @Override
     public void setComplete_name(String complete_name) {
         this.complete_name = complete_name;
@@ -61,7 +49,6 @@ public class Cards implements Global {
     public Direction getDirection() {
         return this.direction;
     }
-
     @Override
     public String toString() {
         return getComplete_name();
