b5e262ea696fc93790ee826c9c7645f863442bc2
revoly afanasev <rewoly@mail.ru>
2024-04-22 00:31:59 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/bureau/Bureau.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/bureau/Bureau.java
@@ -2,68 +2,53 @@ package com.alrosa.staa.gatekeeper_client.model.tree_objects.bureau;
 
 import com.alrosa.staa.gatekeeper_client.model.Direction;
 import com.alrosa.staa.gatekeeper_client.model.tree_objects.Global;
-
 /**
  * Класс для объектов "Бюро"
  */
 public class Bureau implements Global {
     //ID объекта в БД
     private int id;
-
     //Родительский ID объекта в БД
     private int parentId;
-
     //Имя объекта, по умолчанию "Бюро"
     private String complete_name = "Бюро";
-
     private Direction direction = Direction.BUREAU;
-
     public Bureau(){}
-
     public Bureau(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setId(int id) {
         this.id = id;
     }
-
     @Override
     public void setParentId(int parentId) {
         this.parentId = parentId;
     }
-
     @Override
     public int getId() {
         return this.id;
     }
-
     @Override
     public int getParentId() {
         return this.parentId;
     }
-
     @Override
     public String getComplete_name() {
         return complete_name;
     }
-
     @Override
     public void setComplete_name(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
-
     @Override
     public Direction getDirection() {
         return this.direction;
     }
-
     @Override
     public String toString() {
         return getComplete_name();
