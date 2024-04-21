2af8e90549b6b95bd9d29d569587089ee566a8b1
revoly afanasev <rewoly@mail.ru>
2024-04-22 00:37:26 +0900

    first commit

--- gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/computer/computer_objects/Console.java
+++ gatekeeper_client/src/main/java/com/alrosa/staa/gatekeeper_client/model/tree_objects/computer/computer_objects/Console.java
@@ -7,64 +7,49 @@ import com.alrosa.staa.gatekeeper_client.model.tree_objects.Global;
  * Класс для объекта "Консоль"
  */
 public class Console implements Global {
-
     //ID объекта в БД
     private int id;
-
     //Родительский ID объекта в БД
     private int parentId;
-
     //Имя объекта
     private String complete_name = "Консоль";
-
     private Direction direction = Direction.CONSOLE;
-
     public Console(){}
-
     public Console(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setId(int id) {
         this.id = id;
     }
-
     @Override
     public void setParentId(int parentId) {
         this.parentId = parentId;
     }
-
     @Override
     public int getId() {
         return this.id;
     }
-
     @Override
     public int getParentId() {
         return this.parentId;
     }
-
     @Override
     public String getComplete_name() {
         return complete_name;
     }
-
     @Override
     public void setComplete_name(String complete_name) {
         this.complete_name = complete_name;
     }
-
     @Override
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
-
     @Override
     public Direction getDirection() {
         return this.direction;
     }
-
     @Override
     public String toString() {
         return getComplete_name();
