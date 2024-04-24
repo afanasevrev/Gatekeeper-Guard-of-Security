8d6038ad08dee5a9b7d14297b1b3e7b25408418c
unknown <rewoly@mail.ru>
2024-04-24 15:08:23 +0900

    first commit

---
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/ManAdmin.java
@@ -0,0 +1,29 @@
+package com.alrosa.staa.gatekeeper_server.db;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "gk_man_admin")
+public class ManAdmin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "complete_name")
+    private String complete_name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "parent_id")
+    private int parent_id;
+    
+}
