aef5b9d55eb214a88771dbb2fb6d8670946b6b78
unknown <rewoly@mail.ru>
2024-04-24 16:49:03 +0900

    first commit

---
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/PassOffice.java
@@ -0,0 +1,24 @@
+package com.alrosa.staa.gatekeeper_server.db;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@Entity
+@Table(name = "gk_pass_office")
+public class PassOffice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "pass_office_name")
+    private String pass_office_name;
+    @Column(name = "parent_id")
+    private int parent_id;
+    public PassOffice(){}
+    public PassOffice(String pass_office_name, int parent_id) {
+        this.pass_office_name = pass_office_name;
+        this.parent_id = parent_id;
+    }
+}
