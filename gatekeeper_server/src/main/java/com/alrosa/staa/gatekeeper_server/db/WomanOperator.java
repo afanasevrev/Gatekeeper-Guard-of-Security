30f2e479dab4df86833486db502dbfa9e732201d
unknown <rewoly@mail.ru>
2024-04-24 11:51:51 +0900

    first commit

---
+++ gatekeeper_server/src/main/java/com/alrosa/staa/gatekeeper_server/db/WomanOperator.java
@@ -0,0 +1,14 @@
+package com.alrosa.staa.gatekeeper_server.db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "gk_woman_operator")
+public class WomanOperator {
+    
+}
