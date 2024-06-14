3499346df9d89cbc88beccd1be32372d38a3f4a5
unknown <rewoly@mail.ru>
2024-06-14 16:04:09 +0900

    for student

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/WebSocketConfig.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/WebSocketConfig.java
@@ -1,12 +1,15 @@
 package com.alrosa.staa.gatekeeper_perco_driver;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
+@Configuration
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
     }
+    
 }
