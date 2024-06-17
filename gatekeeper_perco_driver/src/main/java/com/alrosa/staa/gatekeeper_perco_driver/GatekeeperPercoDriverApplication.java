c7947d3c698c7ddda9876ebe8d74c7882b58b806
unknown <rewoly@mail.ru>
2024-06-17 17:07:27 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/GatekeeperPercoDriverApplication.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/GatekeeperPercoDriverApplication.java
@@ -1,9 +1,7 @@
 package com.alrosa.staa.gatekeeper_perco_driver;
 
-import com.alrosa.staa.gatekeeper_perco_driver.service.MyWebSocketClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GatekeeperPercoDriverApplication {
 	public static void main(String[] args) {
