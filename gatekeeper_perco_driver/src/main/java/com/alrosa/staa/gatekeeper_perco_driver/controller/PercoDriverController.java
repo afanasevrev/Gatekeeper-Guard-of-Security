16beb85e99be2663d1b17478dd222350aea4e825
unknown <rewoly@mail.ru>
2024-06-18 11:29:42 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/controller/PercoDriverController.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/controller/PercoDriverController.java
@@ -5,8 +5,13 @@ import com.alrosa.staa.gatekeeper_perco_driver.service.PercoDriverWebSocketClien
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class PercoDriverController {
+    private List<String> perco_controllers = new ArrayList<>();
     private Gson gson = new Gson();
     @GetMapping("/")
     private String getConnect() {
