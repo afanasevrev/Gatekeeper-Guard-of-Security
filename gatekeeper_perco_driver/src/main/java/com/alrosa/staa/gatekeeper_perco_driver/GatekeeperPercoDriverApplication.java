cd9b21f0e536751a34d9b960508453e7ef12ec58
unknown <rewoly@mail.ru>
2024-06-19 17:04:44 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/GatekeeperPercoDriverApplication.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/GatekeeperPercoDriverApplication.java
@@ -3,7 +3,8 @@ package com.alrosa.staa.gatekeeper_perco_driver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class GatekeeperPercoDriverApplication {
+public class
+GatekeeperPercoDriverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatekeeperPercoDriverApplication.class, args);
 	}
