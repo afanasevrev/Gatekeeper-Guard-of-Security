9199136598b46b448582b3c8856c45620eeed830
unknown <rewoly@mail.ru>
2024-07-02 14:28:14 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/service/PercoDriverWebSocketHandler.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/service/PercoDriverWebSocketHandler.java
@@ -1,20 +1,22 @@
 package com.alrosa.staa.gatekeeper_perco_driver.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class PercoDriverWebSocketHandler extends TextWebSocketHandler {
+    private Logger logger = Logger.getLogger(PercoDriverWebSocketHandler.class);
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Connection Established");
+        logger.info("Соединение установлено");
     }
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("Received message: " + message.getPayload());
+        logger.info("Получено сообщение: " + message.getPayload());
     }
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println("Connection Closed: " + status);
+        logger.info("Соединение закрыто: " + status);
     }
 }
