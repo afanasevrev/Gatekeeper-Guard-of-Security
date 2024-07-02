f5fdedbe4a865aa0104f5455997e37123310b947
unknown <rewoly@mail.ru>
2024-07-02 15:10:28 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/commands/get_commands/GetNetworkSettings.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/commands/get_commands/GetNetworkSettings.java
@@ -2,6 +2,9 @@ package com.alrosa.staa.gatekeeper_perco_driver.commands.get_commands;
 
 import lombok.Getter;
 import lombok.Setter;
+/**
+ * Класс для отпраки на контроллер команды и в ответ получение сетевых настроек
+ */
 @Getter
 @Setter
 public class GetNetworkSettings {
