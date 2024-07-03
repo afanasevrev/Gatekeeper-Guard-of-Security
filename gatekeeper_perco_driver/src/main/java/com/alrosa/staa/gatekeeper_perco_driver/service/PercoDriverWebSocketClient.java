24abd33b5caba63c317740757a77263f82c4446a
unknown <rewoly@mail.ru>
2024-07-03 09:39:34 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/service/PercoDriverWebSocketClient.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/service/PercoDriverWebSocketClient.java
@@ -25,7 +25,7 @@ public class PercoDriverWebSocketClient extends TextWebSocketHandler {
             this.session = client.doHandshake(this, headers, new URI("ws://" + ip_address + "/tcp")).get();
             logger.info("Подключение установлено к контроллеру: " + ip_address);
         } catch (Exception e) {
-            logger.error("Ошибка подключения к контроллеру" + e);
+            logger.error(e);
         }
     }
     public void sendMessage(String message) {
