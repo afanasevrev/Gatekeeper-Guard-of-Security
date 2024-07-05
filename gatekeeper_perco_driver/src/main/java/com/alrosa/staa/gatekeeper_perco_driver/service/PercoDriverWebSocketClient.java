71396acbd002dde55c4a0db99a46d90bb380bd6f
unknown <rewoly@mail.ru>
2024-07-05 17:25:45 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/service/PercoDriverWebSocketClient.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/service/PercoDriverWebSocketClient.java
@@ -15,9 +15,9 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+/**
+ * Класс устанавливает соединение с контроллером и 
+ */
 public class PercoDriverWebSocketClient extends TextWebSocketHandler {
     private Logger logger = Logger.getLogger(PercoDriverWebSocketClient.class);
     private Gson gson = new Gson();
