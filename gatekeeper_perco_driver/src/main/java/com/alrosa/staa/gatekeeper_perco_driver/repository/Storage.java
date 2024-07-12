40b7cb5a08dfa8a623b84426c90dd9ac56475f69
unknown <rewoly@mail.ru>
2024-07-12 17:11:48 +0900

    first commit

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/repository/Storage.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/repository/Storage.java
@@ -1,7 +1,6 @@
 package com.alrosa.staa.gatekeeper_perco_driver.repository;
 
 import java.util.ArrayList;
-
 public class Storage {
     public static ArrayList<String> storageCards = new ArrayList<>();
 }
