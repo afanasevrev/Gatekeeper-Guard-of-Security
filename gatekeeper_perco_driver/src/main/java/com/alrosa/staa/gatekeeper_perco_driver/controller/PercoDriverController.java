150c800bb9a0cb4144771750e428ba4122a3cf38
unknown <rewoly@mail.ru>
2024-07-16 14:29:30 +0900

    add value

--- gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/controller/PercoDriverController.java
+++ gatekeeper_perco_driver/src/main/java/com/alrosa/staa/gatekeeper_perco_driver/controller/PercoDriverController.java
@@ -6,10 +6,13 @@ import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.ArrayList;
 import java.util.List;
 @RestController
 public class PercoDriverController {
+    private RestTemplate restTemplate = new RestTemplate();
     private List<String> perco_controllers = new ArrayList<>();
     private Gson gson = new Gson();
     @GetMapping("/{ip_address}")
